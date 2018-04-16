02b07ccd4c38305ce7c31093db0b596fa973c901
jefferson <jeff.otoni@gmail.com>
2018-04-16 10:29:12 -0300

    documentation bolt

--- gbolt/gonoverde-gbolt.go
+++ gbolt/gonoverde-gbolt.go
@@ -1,13 +1,15 @@
 /*
+* Go Library (C) 2018 Inc.
 *
-* project server the Upload
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
 *
+* http://www.apache.org/licenses/LICENSE-2.0
+* @project     Noverde
 * @package     main
-* @author      jeffotoni
-* @copyright   Copyright (c) 2017
-* @license     --
-* @link        --
-* @since       Version 0.1
+* @author      @jeffotoni
+* @size        15/04/2018
  */
 
 // We have created some methods below to help us abstract some
